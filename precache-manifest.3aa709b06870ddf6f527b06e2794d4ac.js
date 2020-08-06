0c50e2e10cae8d4fe7cab5d582875ee6caa5c01d
MHD22 <mhd.sad22@gmail.com>
2020-08-06 16:53:52 +0300

    Updates

---
+++ precache-manifest.3aa709b06870ddf6f527b06e2794d4ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0cecd5d977a14ffc46c0fd3208d02e",
+    "url": "/Quote-Generator/index.html"
+  },
+  {
+    "revision": "f131df77fe4ac0080a8f",
+    "url": "/Quote-Generator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d5450cf2123efaa0e83",
+    "url": "/Quote-Generator/static/css/main.08a71489.chunk.css"
+  },
+  {
+    "revision": "f131df77fe4ac0080a8f",
+    "url": "/Quote-Generator/static/js/2.cfd806a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Generator/static/js/2.cfd806a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5450cf2123efaa0e83",
+    "url": "/Quote-Generator/static/js/main.c70b020d.chunk.js"
+  },
+  {
+    "revision": "68cea268cbb09478a60f",
+    "url": "/Quote-Generator/static/js/runtime-main.8f36aec5.js"
+  }
+]);
