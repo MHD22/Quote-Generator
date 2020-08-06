0b67b4eafc2440ae07659760b8362c2891242f93
MHD22 <mhd.sad22@gmail.com>
2020-08-06 18:57:32 +0300

    Updates

---
+++ precache-manifest.2902a8528d57bfff5b23f5cb2be568d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1ca4b2c975e52bacb4ab550d518021",
+    "url": "/Quote-Generator-React-APP/index.html"
+  },
+  {
+    "revision": "f131df77fe4ac0080a8f",
+    "url": "/Quote-Generator-React-APP/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3f54b0b0e7a8e377bb93",
+    "url": "/Quote-Generator-React-APP/static/css/main.08a71489.chunk.css"
+  },
+  {
+    "revision": "f131df77fe4ac0080a8f",
+    "url": "/Quote-Generator-React-APP/static/js/2.cfd806a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Generator-React-APP/static/js/2.cfd806a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f54b0b0e7a8e377bb93",
+    "url": "/Quote-Generator-React-APP/static/js/main.548cbf32.chunk.js"
+  },
+  {
+    "revision": "e77846ac3d02270b5914",
+    "url": "/Quote-Generator-React-APP/static/js/runtime-main.c6547196.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quote-Generator/precache-manifest.3aa709b06870ddf6f527b06e2794d4ac.js"
+  "/Quote-Generator-React-APP/precache-manifest.2902a8528d57bfff5b23f5cb2be568d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Quote-Generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Quote-Generator-React-APP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
